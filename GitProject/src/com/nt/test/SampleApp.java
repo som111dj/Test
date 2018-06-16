4599ed7958e681a8ddd7180bfd7545180139dbde
SOUMYA <SoumyaTL@gmail.com>
2018-06-16 23:35:55 +0530

    1st Change done by Dev2

--- GitProject/src/com/nt/test/SampleApp.java
+++ GitProject/src/com/nt/test/SampleApp.java
@@ -4,8 +4,10 @@ public class SampleApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello !");
-		
+		for(int i=1;i<=10;++i){
+			System.out.println(i);
+		}// end of for loop
 
-	}
+	}//end of main(-)
 
-}
+}// end of class
